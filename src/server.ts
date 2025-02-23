e71988e92cdd8af8e1efd1245204c69d624fb655
unknown <haroldaguirre1991@gmail.com>
2025-02-22 23:08:31 -0500

    Conectar a nuestra bd con nuestro codigo e introduccion a variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express' // ESM Ecmascript modules
+import 'dotenv/config'
 // const express = require('express') // CJS Common JS
 import router from './router'
 // console.log(express)
+import { connectBD } from './config/db'
 
 const app = express()
+
+connectBD()
 
 
 //Leer datos de formularios
