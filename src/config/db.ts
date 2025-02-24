d197e58b7e5a7e5d7756ae3f59a4c4a4868e5ada
unknown <haroldaguirre1991@gmail.com>
2025-02-23 21:54:22 -0500

    Añadiendo Colors a la terminal y creando un Schema para bd

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose"
+import colors from 'colors'
 
 export const connectBD = async () => {
     try {
         const { connection } = await mongoose.connect(process.env.MONGO_URI)
         const url = `${connection.host}:${connection.port}`
         // console.log(connection)
-        console.log(`MongoDB Conectado ${url}`)
+        console.log(colors.cyan.bold(`MongoDB Conectado ${url}`))
     } catch (error) {
-        console.log(error.message)
+        console.log(colors.bgRed.bold(error.message))
         process.exit()
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import colors from 'colors'
 import server from './server'
+
 
 const port = process.env.port || 4000
 
 server.listen(port, ()=>{
-    console.log('Servidor funcionando en el puerto:', port)
+    console.log( colors.bgBlue.magenta.italic(`Servidor funcionando en el puerto: ${port}`) )
 })
