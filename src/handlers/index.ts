c74d6afc05ed3c75ad614ee049b4dc841d068809
unknown <haroldaguirre1991@gmail.com>
2025-03-11 22:54:12 -0500

    Evitar usuarios duplicados

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,6 +2,19 @@ import User from "../models/User"
 import type { Request, Response } from "express"
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    const {email} = req.body
+
+   const userExists = await User.findOne({email})
+
+   if(userExists){
+      const error = new Error('El usuario ya esta registrado')
+     res.status(409).json({error : error.message})
+     return 
+   } 
+
+
+
     const user = new User(req.body)
 
     await user.save()
