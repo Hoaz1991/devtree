2a49c60851e162407eb9a57c661daead359d343b
unknown <haroldaguirre1991@gmail.com>
2025-03-21 00:05:14 -0500

    Hashear passwords de usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,10 @@
 import User from "../models/User"
 import type { Request, Response } from "express"
+import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (req: Request, res: Response) => {
 
-    const {email} = req.body
+    const {email, password} = req.body
 
    const userExists = await User.findOne({email})
 
@@ -16,6 +17,7 @@ export const createAccount = async (req: Request, res: Response) => {
 
 
     const user = new User(req.body)
+    user.password = await hashPassword(password)
 
     await user.save()
 
