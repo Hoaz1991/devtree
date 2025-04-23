b13268384e9e26879e4bd0ddcbddb2a7c1be8fa8
unknown <haroldaguirre1991@gmail.com>
2025-04-23 00:27:00 -0500

    Añadiendo el Handle para las cuentas

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,6 @@
-import User from "../models/User"
 import type { Request, Response } from "express"
+import slugify from 'slugify';
+import User from "../models/User"
 import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (req: Request, res: Response) => {
@@ -7,17 +8,26 @@ export const createAccount = async (req: Request, res: Response) => {
     const {email, password} = req.body
 
    const userExists = await User.findOne({email})
-
    if(userExists){
-      const error = new Error('El usuario ya esta registrado')
+      const error = new Error('Email registrado con otro usuario')
+     res.status(409).json({error : error.message})
+     return 
+   } 
+   
+   const handle = slugify(req.body.handle, '')
+   const handleExists = await User.findOne({handle})
+   if(handleExists){
+      const error = new Error('Nombre de usuario no disponible')
      res.status(409).json({error : error.message})
      return 
    } 
 
 
-
     const user = new User(req.body)
     user.password = await hashPassword(password)
+    user.handle =  handle
+
+    console.log(slugify(handle, '_'))
 
     await user.save()
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
-interface IUser {
-   name: string,
-   email: string,
+export interface IUser {
+   handle: string
+   name: string
+   email: string
    password: string
 }
 
 const userSchema = new Schema({
+    handle:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true
+
+    },
     name:{
         type: String,
         required: true,
@@ -17,7 +26,8 @@ const userSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password:{
         type: String,
