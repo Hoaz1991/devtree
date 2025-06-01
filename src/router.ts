d8057d841ec4199ebd4636128904b4c1588b4a98
Hoaz1991 <haroldaguirre1991@gmail.com>
2025-06-01 01:05:37 -0500

    Validación con Express Validator - Primeros Pasos

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import User from './models/User'
 import { createAccount } from './handlers'
 
 const router = Router()
 
 //  ** Autenticacion y registro
-router.post('/auth/register', createAccount)
+router.post('/auth/register', 
+    body('handle').notEmpty(),
+    createAccount)
 
  export default router
